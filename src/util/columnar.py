e133848963eea79dda9cf19c70ae497ca37dfa9d
Blit <107967397+BunnyBlit@users.noreply.github.com>
2023-12-27 12:15:50 -0800

    starting to write some useful functions for dealing with CSVs

--- src/util/columnar.py
+++ src/util/columnar.py
@@ -1,9 +1,12 @@
 """Column-like serialization for data
 """
 from abc import ABC
-from typing import Dict
+from typing import Dict, List
 import csv
 from pathlib import Path
+import re
+
+import attr
 
 class Columnar(ABC):
     """ Base class for holding onto some simple ops for doing data manipulation
@@ -12,6 +15,7 @@ class Columnar(ABC):
         class.attr = [value, value, value], where type(value) == self.taxonomy[attr]
     """
     taxonomy: Dict
+    __hidden_attr_regex: re.Pattern
     
     def __init__(self, taxonomy):
         """ Constructor! You also need to pass a taxonomy of types
@@ -22,6 +26,7 @@ class Columnar(ABC):
                 tells us what the type of each column is, and provides a way to
                 serialize to that type
         """
+        self.__hidden_attr_regex = re.compile(r"^_")
         self.__check_taxonomy(taxonomy)
         self.taxonomy = taxonomy
         self.__check_init()
@@ -34,7 +39,7 @@ class Columnar(ABC):
                 {"name": "sam", "number": 5, ...}
         """
         self.__check_dim(data)
-        for key, value in data:
+        for key, value in data.items():
             self.__check_data(key, value)
             value_type = self.taxonomy[key]
             self[key].append(value_type(value))
@@ -61,10 +66,26 @@ class Columnar(ABC):
         Args:
             taxonomy (Data[str, type]): a dictionary that tells us what type each of our columns has
         """
-        attrs = vars(self)
-        for key in taxonomy.keys():
-            assert key in attrs
+        
+        attrs_set = set(self.__extract_attribute_names())
+        taxonomy_set = set(taxonomy.keys())
+        # the venn diagram should be a perfect circle
+        if len(attrs_set) != len(attrs_set.union(taxonomy_set)):
+            print(f"Attributes: {attrs_set}")
+            print(f"Taxonomy: {taxonomy_set}")
+            assert False
     
+    def __extract_attribute_names(self) -> List:
+        """Pull out attribute names for the class.
+
+        Returns:
+            List: a list of all the attribute names defined on this class
+                subclasses 
+        """
+        cls_ref_keys = [key for key in vars(type(self)).keys()]
+        attrs = [key for key in cls_ref_keys if not self.__hidden_attr_regex.match(key)]
+        return attrs
+
     def __check_data(self, key:str, value):
         """ For a particular column name and value, check that the column exists and
             that the type of the taxonomy lines up with the type of the value
@@ -72,11 +93,9 @@ class Columnar(ABC):
             key (str): column name
             value (Any): the value to add to that column
         """
-        attrs = vars(self)
+        attrs = self.__extract_attribute_names()
         assert key in attrs
-
         assert key in self.taxonomy
-        assert type(self.taxonomy[key]) == type(value)
     
     def __check_dim(self, data:Dict):
         """ Check the dimensionality (cardinality?) of the row of data coming in--
@@ -86,15 +105,16 @@ class Columnar(ABC):
             data (Dict[str, Any]): a "row" of data, as a dictionary. For example:
                 {"name": "sam", "number": 5, ...}
         """
-        assert len(vars(self).keys()) == len(data.keys())
+        attrs = self.__extract_attribute_names()
+        assert len(attrs) == len(data.keys())
     
     def __check_init(self):
         """ Check that we've initialized a subclass correctly. All attributes must have
             the 'list' type
         """
-        attrs = vars(self)
-        for value in attrs.values():
-            assert type(value) == list
+        attrs = self.__extract_attribute_names()
+        for name in attrs:
+            assert type(self[name]) == list
         
     def __getitem__(self, key):
         """ Get an item from the serializable. Lets us use class[name] syntax to get the
